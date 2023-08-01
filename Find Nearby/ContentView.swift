7d0c22cc1a32bb8d0a1a7a137f7a2d9533b17114
Yuhan Zhang <121049397+ZzhangYH@users.noreply.github.com>
2023-08-01 14:39:24 +0800

    Initial Commit

---
+++ Find Nearby/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Find Nearby
+//
+//  Created by Yuhan Zhang on 2023/8/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Find Nearby/Find_NearbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Find_NearbyApp.swift
+//  Find Nearby
+//
+//  Created by Yuhan Zhang on 2023/8/1.
+//
+
+import SwiftUI
+
+@main
+struct Find_NearbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
